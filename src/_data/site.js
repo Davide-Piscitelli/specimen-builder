05715846c72e99053a13730b6457a08d7dd87629
Davide Piscitelli <davidepscll@MacBook-Air-di-Davide.local>
2026-05-29 17:08:26 +0200

    Rimossa duplicazione testo in site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -12,12 +12,12 @@ module.exports = {
 	description:
 		"A design built on top of Specimen Skeleton, based on research insights into effective digital type specimens.", // The description of your type specimen.
 
-	typeface: "STIX fonts", // The name of the typeface you are building
-	link: "https://www.stixfonts.org", // This is the URL used for the button
+	typeface: "Tecnica Typeface", // The name of the typeface you are building
+	link: "https://github.com/Davide-Piscitelli/Tecnica-Typeface", // This is the URL used for the button
 	about:
-		"The mission of the Scientific and Technical Information Exchange (STIX) font creation project is the preparation of a comprehensive set of fonts that serve the scientific and engineering community in the process from manuscript creation through final publication, both in electronic and print formats.", // All about the typeface
-	designers: "Tiro Typeworks", // And who designed it
-	coverage: "Latin, Greek, & Cyrillic", // This populates the link that anchors to the list of languages.
+		"Tecnica 55 and Tecnica 53 are bitmap-inspired typefaces based on dot-matrix printer grids. Tecnica 55 utilizes a 5×5 grid to deliver bold, structured, and high-impact letterforms ideal for branding and interfaces. Conversely, Tecnica 53 uses a compact 5×3 grid for a narrower, space-saving, and minimalist aesthetic. Both fonts include linear variants called '55 Line' and '53 Line.' Together, they form a versatile, retro-futuristic typographic system tailored for digital interfaces, developer tools, and tech branding. They perfectly balance technical geometric precision with functional readability.", // All about the typeface
+	designers: "Davide Piscitelli", // And who designed it
+	coverage: "Latin & Cyrillic", // This populates the link that anchors to the list of languages.
 	language:
 		"Afrikaans, Albanian, Asturian, Basque, Bosnian, Breton, Catalan, Cebuano, Cornish, Corsican, Croatian, Czech, Danish, English, Esperanto, Estonian, Faroese, Faroese, Filipino, Finnish, Flemish, French, Frisian, Friulian, Gaelic, Galician, German, Greenlandic, Guarani, Haitian, Hawaiian, Hiligaynon, Hungarian, Icelandic, Igbo, Indonesian, Irish, Italian, Kurdish, Latin, Latvian, Lithuanian, Livonian, Luxembourgish, Malagasy, Malay, Maltese, Maori, Moldavian, Nederlands, Norwegian, Occitan, Polish, Portuguese, Provencal, Romanian, Romansch, Saami, Samoan, Scots, Scottish, Slovak, Slovenian, Spanish, Swahili, Swedish, Tagalog, Turkish, Walloon, Welsh, Wolof, Belarusian, Bulgarian, Macedonian, Moldavian, Russian, Serbian (Cyrillic), Ukrainian, Greek.",
 
